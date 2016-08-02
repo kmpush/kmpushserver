6794f1ee80f06df69f8a4cdab89339f40f5a1f76
rjgc2015 <550296705@qq.com>
2016-08-02 16:32:18 +0800

    shao2.2

--- pushServer/Application/Home/Model/TblMqlogModel.class.php
+++ pushServer/Application/Home/Model/TblMqlogModel.class.php
@@ -12,8 +12,16 @@ use Think\Model;
 class TblMqlogModel extends Model
 {
     protected $connection = 'DB_CONFIG2';
+    //单独商家折线图
     public function getInfo($companyCode)
     {
         return $this->query("SELECT companycode,convert(CreateTime,date) as date,max(TIMESTAMPDIFF(SECOND,CreateTime,ReceiveTime)) as max,min(TIMESTAMPDIFF(SECOND,CreateTime,ReceiveTime)) as min,avg(TIMESTAMPDIFF(SECOND,CreateTime,ReceiveTime)) as avg FROM kmgate.km_tbl_mqlog where companycode='$companyCode' group by convert(CreateTime,date);");
+    }
+    //单独商家超过平均值OR最大值OR最小值推送详情
+    public function getMoreInfo($companyCode,$time,$num,$page){
+        return $this->where("companycode='$companyCode' and convert(CreateTime,date)='$time' and timestampdiff(second,CreateTime,ReceiveTime)>='$num'")->field("CreateTime,ReceiveTime,timestampdiff(second,CreateTime,ReceiveTime) as date,companycode,SourceSystem")->page($page,10)->select();
+    }
+    public function getCount($companyCode,$time,$num){
+        return $this->where("companycode='$companyCode' and convert(CreateTime,date)='$time' and timestampdiff(second,CreateTime,ReceiveTime)>='$num'")->count();
     }
 }
