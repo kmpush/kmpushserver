3ff470b7e8099fcbb9fa5eb20521c979c29d179f
huifang-starnet <luhuifang@star-net.cn>
2016-08-04 16:40:01 +0800

    fang1.8

--- pushServer/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ pushServer/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -26,11 +26,6 @@
 <!--左菜单栏-->
 <div class="left_div">
     <div class="ui vertical menu" id="left_menu">
-<<<<<<< HEAD
-=======
-        <!--<div class="ui vertical pointing menu" id="left_menu">-->
-        <div class="item">
->>>>>>> 5f1288acf57141c863095c4bd8ccb936137a0602
             <br/><br/>
             <a class="item active" href="/pushServer/index.php/Home/Index/info"> <strong>推送量</strong> </a>
             <a class="item" href="javascript:go_here('a1')"> 平台推送量对比 </a>
@@ -51,11 +46,13 @@
 </div>
 <div class="right_content">
     <!--顶部导航菜单-->
-    <br/><div id="header"><span style="border-bottom:2px solid #2D96E9;font-size: 2.5em;font-family:KaiTi_GB2312; font-weight:bold">K米平台服务质量监测系统</span></div><br/>
-    <div class="ui tabular menu ">
-        <a class="item active">推送量 </a>
-        <a class="item" href="/pushServer/index.php/Home/Index/time_delay_info">推送时延 </a>
-        <a class="item" href="/pushServer/index.php/Home/Index/company_info">商家详情 </a>
+    <div style="background:url('/pushServer/Public/css/image/bluesky.jpg') no-repeat center;background-size:100%">
+        <br/><div id="header"><span style="padding:20px; border-bottom:2px solid #2D96E9;font-size: 2.5em;font-family:KaiTi_GB2312; font-weight:bold">K米平台服务质量监测系统</span></div><br/>
+        <div class="ui tabular menu " style="padding-left:30px">
+            <a class="item active">推送量 </a>
+            <a class="item" href="/pushServer/index.php/Home/Index/time_delay_info"><strong>推送时延</strong> </a>
+            <a class="item" href="/pushServer/index.php/Home/Index/company_info"><strong>商家详情</strong> </a>
+        </div>
     </div>
 
     <div id="a1">
@@ -66,7 +63,7 @@
             <input type="text" id="to" name="to" style="width:105px;height: 25px;font-size: 20px">
             <button class="blue ui button" onclick="PostNumData()">查询</button>
         </div>
-        <br><br>
+        <br><br> <br>
         <span style="border-bottom:2px solid #2D96E9;font-size: 20px;font-weight:bold">推送量</span>
 
         <div class="ui Inverted divider"></div>
--- pushServer/Application/Runtime/Cache/Home/8bfe5cc91513fbb7ccbc5a9bdffbd028.php
+++ pushServer/Application/Runtime/Cache/Home/8bfe5cc91513fbb7ccbc5a9bdffbd028.php
@@ -25,14 +25,12 @@
 <!--左菜单栏-->
 <div class="left_div">
     <div class="ui vertical menu" id="left_menu">
-
             <br/><br/>
             <a class="item active" href="/pushServer/index.php/Home/Index/info" > <strong>推送量</strong> </a>
             <a class="item" href="/pushServer/index.php/Home/Index/info"> 平台推送量对比 </a>
             <a class="item" href="/pushServer/index.php/Home/Index/info"> 推送量城市分布 </a>
             <a class="item" href="/pushServer/index.php/Home/Index/info"> 推送平台占比 </a>
             <a class="item" href="/pushServer/index.php/Home/Index/info"> 推送详细记录 </a>
-        
         
             <a class="item active" href="/pushServer/index.php/Home/Index/time_delay_info"> <strong>推送时延</strong> </a>
             <a class="item" href="javascript:go_here('a1')"> 日推送时延 </a>
@@ -43,17 +41,19 @@
             <a class="item active" href="/pushServer/index.php/Home/Index/company_info"> <strong>商家详情</strong> </a>
             <a class="item" href="/pushServer/index.php/Home/Index/company_info"> 商家推送时延 </a>
             <a class="item" href="/pushServer/index.php/Home/Index/company_info"> 商家推送详细记录 </a>
-        
     </div>
 </div>
 <div class="right_content">
     <!--顶部导航菜单-->
-    <br/><div id="header"><span style="border-bottom:2px solid #2D96E9;font-size: 2.5em;font-family:KaiTi_GB2312; font-weight:bold">K米平台服务质量监测系统</span></div><br/>
-    <div class="ui tabular menu ">
-        <a class="item" href="/pushServer/index.php/Home/Index/info">推送量 </a>
-        <a class="item active" >推送时延</a>
-        <a class="item" href="/pushServer/index.php/Home/Index/company_info">商家详情 </a>
+    <div style="background:url('/pushServer/Public/css/image/bluesky.jpg') no-repeat center;background-size:100%">
+        <br/><div id="header"><span style="padding:20px;border-bottom:2px solid #2D96E9;font-size: 2.5em;font-family:KaiTi_GB2312; font-weight:bold">K米平台服务质量监测系统</span></div><br/>
+        <div class="ui tabular menu " style="padding-left:30px">
+            <a class="item" href="/pushServer/index.php/Home/Index/info"><strong>推送量</strong> </a>
+            <a class="item active" >推送时延</a>
+            <a class="item" href="/pushServer/index.php/Home/Index/company_info"><strong>商家详情</strong> </a>
+        </div>
     </div>
+
     <div id="a1">
         <div style="float: right">
             <label for="from" style="font-size: 20px">开始时间</label>
@@ -62,7 +62,7 @@
             <input type="text" id="to" name="to" style="width:105px;height: 25px;font-size: 20px">
             <button class="blue ui button" onclick="PostTimeDelayData()">查询</button>
         </div>
-        <br><br>
+        <br><br><br>
         <span style="border-bottom:2px solid #2D96E9;font-size: 20px;font-weight:bold">推送时延</span>
 
         <div class="ui Inverted divider"></div>
